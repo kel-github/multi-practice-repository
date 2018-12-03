eb3a866f31898199d04c941f7be5b4b6ae9ecccb
Kelly Garner <getkellygarner@gmail.com>
2018-12-03 14:38:11 +1000

    refreshed run dcm processing folder

--- run_dcms_code/spm_dcm_sess1_anatROI_initGLM_run/contrast_beta.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'/home/kgarner/mult-conn/repos/multi-task-train-conn-change/DCM_code_cluster/spm_dcm_sess1/contrast_beta_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
--- run_dcms_code/spm_s1s2_dcm_anatROI_flGLM_winS1B/glm_est.m
+++
@@ -1,8 +0,0 @@
-%-----------------------------------------------------------------------
-% Job saved on 02-Jul-2015 13:58:40 by cfg_util (rev $Rev: 6460 $)
-% spm SPM - SPM12 (6470)
-% cfg_basicio BasicIO - Unknown
-%-----------------------------------------------------------------------
-matlabbatch{1}.spm.stats.fmri_est.spmmat = '<UNDEFINED>';
-matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
-matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
