f5e31e139a4fe2af09b9a00a5b1a781c2aeb2301
kel-github <getkellygarner@gmail.com>
2018-12-05 09:08:58 +1000

    added s1 run dcm code

---
+++ run_dcms_code/spm_dcm_sess1_anatROI_initGLM_run/ReadMe.txt
@@ -0,0 +1,12 @@
+Multitasking/DCM Analysis notes - Mon 21st May 2018
+Analysis code run using fmri_euramoo_vAnat.pbs and dcm_analysis_anat_mask_vers.m
+Dependent on spm12
+Data backups stored in ADD 
+
+Subs with errors during model fitting:
+sub 128: SESSION 1 IMAGES ARE MISSING
+sub 138: SESSION 2 FILES ARE MISSING
+sub 223: SESSION 1 IMAGES ARE MISSING
+
+
+
---
+++ run_dcms_code/spm_dcm_sess1_anatROI_initGLM_run/contrast_beta_job.m
@@ -0,0 +1,18 @@
+%-----------------------------------------------------------------------
+% Job saved on 28-May-2018 10:36:38 by cfg_util (rev $Rev: 6942 $)
+% spm SPM - SPM12 (7219)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.con.spmmat = {'/home/kgarner/mult-conn/repos/multi-task-train-conn-change/dcm-out/dcm_analysis_v5/sub_111_out/FSTL_GLM/SPM.mat'};
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.name = 'sing-open';
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.weights = [1 0
+                                                        0 1];
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.delete = 0;
+
+matlabbatch{2}.spm.stats.con.spmmat = {'/home/kgarner/mult-conn/repos/multi-task-train-conn-change/dcm-out/dcm_analysis_v5/sub_111_out/FSTL_GLM/SPM.mat'};
+matlabbatch{2}.spm.stats.con.consess{1}.fcon.name = 'sing-open';
+matlabbatch{2}.spm.stats.con.consess{1}.fcon.weights = [1 0
+                                                        0 1];
+matlabbatch{2}.spm.stats.con.consess{1}.fcon.sessrep = 'none';
+matlabbatch{2}.spm.stats.con.delete = 0;
