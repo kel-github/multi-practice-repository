957458c395e141a5b09400451caad5798cfd4989
kel-github <getkellygarner@gmail.com>
2018-12-05 09:41:12 +1000

    added s1s2 mt run dcm code

---
+++ run_dcms_code/spm_s1s2_dcm_anatROI_flGLM_winS1B_regOutSing/dcm_def_contrasts_wSESS_v3.m
@@ -0,0 +1,19 @@
+%-----------------------------------------------------------------------
+% Job saved on 31-Jan-2016 16:32:15 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6685)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+% v2 includes only effects of interest contrast
+% v3 adjusts for effects of interest that inc multi, practice
+matlabbatch{1}.spm.stats.con.spmmat = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.name = 'open-sing';
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.weights = eye(2);
+matlabbatch{1}.spm.stats.con.consess{1}.fcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.delete = 0;
+
+matlabbatch{1}.spm.stats.con.consess{2}.fcon.name = 'effectsOfInt';
+matlabbatch{1}.spm.stats.con.consess{2}.fcon.weights = eye(4);
+matlabbatch{1}.spm.stats.con.consess{2}.fcon.weights(1,:) = 0;
+matlabbatch{1}.spm.stats.con.consess{2}.fcon.weights(2,:) = 0;
+matlabbatch{1}.spm.stats.con.consess{2}.fcon.sessrep = 'none';
+matlabbatch{1}.spm.stats.con.delete = 0;
---
+++ run_dcms_code/spm_s1s2_dcm_anatROI_flGLM_winS1B_regOutSing/gen_b_mats_v5.m
@@ -0,0 +1,34 @@
+clear all
+%%% K. Garner, May 21st 2018. Defining b matrix space
+% have written new function as old one had removed some of the combinations in error
+% saves the b_mats in a file called b_mats_v3
+% v4 - excludes the parameters that did not survive the session 1 analysis
+%           from
+%           lipl lput smfc
+%to lipl    1     4    7
+%   lput    2     5    8
+%   smfc    3     6    9
+%%%%% not included
+%%%%% lipl_to_smfc = 3; 
+%%%%% smfc_to_lput = 8;
+% redid on 18th November 2018 in light of new results
+smfc_to_lipl = 7;
+lipl_to_lput = 2;
+lput_to_lipl = 4;
+lput_to_smfc = 6;
+
+conns = [smfc_to_lipl, lipl_to_lput, lput_to_lipl, lput_to_smfc];
+base = zeros(3,3);
+b_mats = [];
+count_mats = 0;
+for count = 1:length(conns)
+    a = nchoosek(conns, count);
+    for perms = 1:size(a,1)
+        count_mats = count_mats + 1;
+        tmp = base;
+        tmp(a(perms,:)) = 1;
+        b_mats(:,:,count_mats) = tmp;
+    end
+end
+
+save('b_mats_v5', 'b_mats');
